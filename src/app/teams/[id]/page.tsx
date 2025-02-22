0024a9436e87e1d2a1d0a92a7b4a552d115c986b
ekrema <ekremamass@gmail.com>
2025-02-23 00:27:26 +0200

    redesigned tasks dt in team page

--- src/app/teams/[id]/page.tsx
+++ src/app/teams/[id]/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { auth } from "@/lib/auth";
 import UserCard from "@/components/users/UserCard";
 import { getUserTeamRole } from "@/actions/user";
+import { DataTable } from "@/components/ui/data-table";
+import { columns as taskColumns } from "@/app/tasks/columns";
 
 export default async function TeamPage({ params }: { params: { id: string } }) {
   try {
@@ -40,18 +42,36 @@ export default async function TeamPage({ params }: { params: { id: string } }) {
       })
     );
 
+    const tasks = await prisma.task.findMany({
+      where: { teamId: team.id },
+    });
+
     return (
       <main>
         <h1 className="mb-4 text-xl md:text-2xl">Team {`${team.name}`}</h1>
-        <div>
+        <div className="mb-8">
           <h2>Members</h2>
-          {membersWithRoles.map((member) => (
-            <UserCard
-              key={member.user.id}
-              user={member.user}
-              teamRole={member.role}
-            />
-          ))}
+          {membersWithRoles.length > 0 ? (
+            membersWithRoles.map((member) => (
+              <UserCard
+                key={member.user.id}
+                user={member.user}
+                teamRole={member.role}
+              />
+            ))
+          ) : (
+            <div>No members</div>
+          )}
+        </div>
+        <div>
+          <h2>Tasks</h2>
+          {tasks.length > 0 ? (
+            <div className="container mx-auto py-10">
+              <DataTable columns={taskColumns} data={tasks} />
+            </div>
+          ) : (
+            <div>No tasks</div>
+          )}
         </div>
       </main>
     );
